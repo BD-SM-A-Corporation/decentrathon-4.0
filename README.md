2374eea6a71b01168d87f29334642916803aea78
artcevvv <artcevvv@yandex.kz>
2025-09-13 17:14:50 +0500

    qwer

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Architecture
 
 Donut (pre-trained) -> PaddleOCR -> Gemini (Post-processing)
+
+## Remark
+
+ABOUT PDFs:
+
+PPStructureV3 - "Intelligently converts complex PDFs and document images into Markdown and JSON files that preserve original structure. Outperforms numerous commercial solutions in public benchmarks. Perfectly maintains document layout and hierarchical structure."
--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-from PU
+from PIL import Image
+from transformers import pipeline
+from paddleocr import PPStructureV3
+
+# Donut (pre-trained)
+# https://huggingface.co/naver-clova-ix/donut-base-finetuned-cord-v2
+
+image = Image.open("image.png") # Change to the input
+
+donut_pipe = pipeline("image-to-text", model="naver-clova-ix/donut-base-finetuned-cord-v2")
+
+# PaddleOCR  !!! PPStructureV3 !!!
+paddleocr_pipeline = PPStructureV3(use_angle_cls=True, lang="ru")
+
+result = paddleocr_pipeline.predict(input=image)
+
+for res in result:
+    res.print()
+    res.save_to_json(save_path="result")
+    res.save_to_markdown(save_path="result")
+    
+
+
+# Gemini (Post-processing)
+# gemini = Gemini(model_name="gemini-2.0-flash")
+
+
+
