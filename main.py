e6ddc04746977eec7b17758436d16662754624f1
artcevvv <artcevvv@yandex.kz>
2025-09-14 16:44:47 +0500

    qwer

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 from classes import Preprocessor, Postprocessor, DonutOCR
 
 SAMPLES_FOLDER_PATH = "./decentrathon_samples"
-image = f"{SAMPLES_FOLDER_PATH}/8A16.pdf" 
+image = f"{SAMPLES_FOLDER_PATH}/beta.jpg" 
 
 
 
@@ -15,12 +15,21 @@ preprocessor = Preprocessor()
 # cv2.imwrite(output_image_path, preprocessed_image)
 
 # PaddleOCR: extract text and structure
+# The code snippet `paddle_result = preprocessor.predict(image)` is using a preprocessor object to
+# predict and extract text and structure from the given image. The result is stored in the
+# `paddle_result` variable, which likely contains information about the text and structure found in
+# the image.
 paddle_result = preprocessor.predict(image)
 
-for paddle_res in paddle_result:
-    print(paddle_res)
-    paddle_res.save_to_markdown("result")
+print(str(paddle_result) + "\n\n")
 
+for res in paddle_result:
+    # idk how to extract text only
+    print(str(res['parsing_res_list']) + "\n\n")
+    res.save_to_json("output")
+    res.save_to_markdown("output")
+            
+# Does not work well for now
 # Donut: ask a question for understanding
 # donut = DonutOCR()
 # question = "Какая дата указана в документе?"
@@ -29,6 +38,7 @@ for paddle_res in paddle_result:
 # with open("log_gemini_input.txt", "w") as f:
 #     f.write(str(donut_result))
 
+# Imlpement a post-processing
 # Gemini (Post-processing)
 # postprocessor = Postprocessor(model_name="gemini-2.0-flash")
 # final_output = postprocessor.postprocess(combined_input)
